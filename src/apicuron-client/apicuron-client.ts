24f8b114d383c558c56eb31d55e978f13aefb90a
Mehdi-SK <zoubirimehdi64@gmail.com>
2026-01-27 16:28:54 +0100

    refactor(apicuron-client): implement strategy usage to send reports to APICURON

--- src/apicuron-client/apicuron-client.ts
+++ src/apicuron-client/apicuron-client.ts
@@ -3,82 +3,59 @@
 import { Logger } from '../logger.js'
 import { ApicuronConfig } from '../types/input.types.js'
 import { Report } from '../types/report.schema.js'
+import { Environment } from './const.js'
+import { BulkSendStrategy } from './strategies/bulk-send.strategy.js'
+import { SendStrategy } from './strategies/send-strategy.interface.js'
 
 export class APICURONClient {
-  private endpoint: string
-  private token: string
-  private logger = new Logger('APICURONClient')
-  constructor(config: ApicuronConfig) {
-    const endpoints = {
-      prod: 'https://apicuron.org/api/reports',
-      dev: 'https://dev.apicuron.org/api/reports'
-    }
-    this.endpoint = endpoints[config.environment]
-    this.token = config.apicuron_token
-  }
-
-  async sendReports(reports: Report[]): Promise<void> {
-    if (reports.length === 0) {
-      this.logger.info('No valid commits to process')
-      return
+    private environment: Environment
+    private token: string
+    private logger = new Logger('APICURONClient')
+    constructor(config: ApicuronConfig) {
+        this.environment = config.environment
+        this.token = config.apicuron_token
     }
 
-    try {
-      this.logger.info(`Sending ${reports.length} reports to ${this.endpoint}`)
+    async sendReports(reports: Report[]): Promise<void> {
+        if (reports.length === 0) {
+            this.logger.info('No valid commits to process')
+            return
+        }
+        const strategy = this.selectSendStrategy(reports)
 
-      const requestBody = { reports }
-      // In your frontend code before sending
-      await this.measurePayloadSize(reports)
-      const response = await fetch(this.endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.token}`,
-          version: '2'
-        },
-        body: JSON.stringify(requestBody)
-      })
+        try {
+            this.logger.info(
+                `Sending ${reports.length} reports to ${this.environment}`
+            )
+            strategy.sendReports(reports)
+            this.logger.info(`Successfully sent ${reports.length} reports`)
+        } catch (error) {
+            this.logger.error('Failed to send reports')
+            if (error instanceof Error) {
+                this.logger.error(error.stack || error.message)
+            }
+            throw error
+        }
+    }
 
-      const contentType = response.headers.get('content-type') || ''
-      const isJson = contentType.includes('application/json')
-      const responseBody = isJson
-        ? await response.json()
-        : await response.text()
+    selectSendStrategy(reports: Report[]): SendStrategy {
+        return new BulkSendStrategy({
+            environment: this.environment,
+            apiToken: this.token
+        })
+    }
 
-      if (!response.ok) {
-        this.logger.error(
-          `API Error: ${response.status} ${response.statusText}`
+    private async measurePayloadSize(reports: Report[]): Promise<number> {
+        const payload = { reports: [...reports] } // your 415 reports
+        const payloadString = JSON.stringify(payload)
+        const sizeInBytes = new Blob([payloadString]).size
+        const sizeInKB = sizeInBytes / 1024
+        const sizeInMB = sizeInKB / 1024
+
+        this.logger.info(
+            `Payload size: ${sizeInBytes} bytes (${sizeInKB.toFixed(2)} KB, ${sizeInMB.toFixed(2)} MB)`
         )
-        this.logger.error(`Response body: ${JSON.stringify(responseBody)}`)
-        throw new Error(
-          `API request failed: ${response.status} ${response.statusText}`
-        )
-      }
 
-      this.logger.info(`Successfully sent ${reports.length} reports`)
-      this.logger.debug(`API Response: ${JSON.stringify(responseBody)}`)
-    } catch (error) {
-      this.logger.error('Failed to send reports')
-      if (error instanceof Error) {
-        this.logger.error(error.stack || error.message)
-      }
-      throw error
+        return sizeInKB
     }
-  }
-
-  async bulkSendJsonReports(reports: Report[]): Promise<void> {}
-
-  private async measurePayloadSize(reports: Report[]): Promise<number> {
-    const payload = { reports: [...reports] } // your 415 reports
-    const payloadString = JSON.stringify(payload)
-    const sizeInBytes = new Blob([payloadString]).size
-    const sizeInKB = sizeInBytes / 1024
-    const sizeInMB = sizeInKB / 1024
-
-    this.logger.info(
-      `Payload size: ${sizeInBytes} bytes (${sizeInKB.toFixed(2)} KB, ${sizeInMB.toFixed(2)} MB)`
-    )
-
-    return sizeInKB
-  }
 }
