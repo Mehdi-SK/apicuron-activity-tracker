3ffe740f0a16bac2636a03b3b85057b1f6af0596
Mehdi-SK <zoubirimehdi64@gmail.com>
2026-01-27 16:28:28 +0100

    refactor(apicuron-client): refactor DirectSendStrategy to extend new abstract class structure

--- src/apicuron-client/strategies/direct-send.strategy.ts
+++ src/apicuron-client/strategies/direct-send.strategy.ts
@@ -1,20 +1,16 @@
-import { Logger } from '../../logger.js'
 import { Report } from '../../types/report.schema.js'
 import { EndpointMap } from '../const.js'
 import { SendContext, SendStrategy } from './send-strategy.interface.js'
 
-export class DirectSendStrategy implements SendStrategy {
-    private endpoint: string
-    private token: string
-    private logger = new Logger('DirectSendStrategy')
+export class DirectSendStrategy extends SendStrategy {
+    private apiUrl: string
     constructor(context: SendContext) {
-        this.endpoint = EndpointMap[context.environment] + 'reports'
-        this.token = context.apiToken
+        super(context)
+        this.apiUrl = EndpointMap[context.environment] + 'reports'
     }
-
-    async sendReports(reports: Report[]): Promise<void> {
+    async sendReports(reports: Report[]): Promise<Response> {
         const requestBody = { reports }
-        const response = await fetch(this.endpoint, {
+        const response = await fetch(this.apiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -27,18 +23,18 @@ export class DirectSendStrategy implements SendStrategy {
         const contentType = response.headers.get('content-type') || ''
         const isJson = contentType.includes('application/json')
         const responseBody = isJson
-        ? await response.json()
-        : await response.text()
+            ? await response.json()
+            : await response.text()
 
-        
-      if (!response.ok) {
-        this.logger.error(
-          `API Error: ${response.status} ${response.statusText}`
-        )
-        this.logger.error(`Response body: ${JSON.stringify(responseBody)}`)
-        throw new Error(
-          `API request failed: ${response.status} ${response.statusText}`
-        )
-      }
+        if (!response.ok) {
+            this.logger.error(
+                `API Error: ${response.status} ${response.statusText}`
+            )
+            this.logger.error(`Response body: ${JSON.stringify(responseBody)}`)
+            throw new Error(
+                `API request failed: ${response.status} ${response.statusText}`
+            )
+        }
+        return response
     }
 }
